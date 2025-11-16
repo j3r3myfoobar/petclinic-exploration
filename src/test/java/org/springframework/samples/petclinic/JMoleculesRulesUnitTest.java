9696b537594eccc631c95350018e63a12f6b9e94
ox00004a <ox00004a@gmail.com>
2025-11-16 22:17:52 +0100

    First step. Layered architecture validation + DDD

---
+++ src/test/java/org/springframework/samples/petclinic/JMoleculesRulesUnitTest.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.jmolecules.archunit.JMoleculesDddRules;
+
+@AnalyzeClasses(packages = "org.springframework.samples.petclinic")
+public class JMoleculesRulesUnitTest {
+
+	@ArchTest
+	ArchRule dddRules = JMoleculesDddRules.all();
+
+	@ArchTest
+	ArchRule layeredArchitecture = JMoleculesArchitectureRules.ensureLayering();
+
+}

7854784d068e9e19c2e891eac6c7e91d5b1bd737
ox00004a <ox00004a@gmail.com>
2025-11-19 12:04:09 +0000

    Add @Transactional to VetController to fix LazyInitializationException

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -22,6 +22,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,6 +37,7 @@ import org.jmolecules.architecture.layered.InterfaceLayer;
  */
 @InterfaceLayer
 @Controller
+@Transactional(readOnly = true)
 class VetController {
 
 	private final VetRepository vetRepository;
